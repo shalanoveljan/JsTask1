f76ad3ffa0f999b0d4ea4ccf335580c039206bdb
shalanoveljan <eljanash@code.edu.az>
2023-09-13 11:35:07 +0400

    addedd

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 let name="Huseyn";
+let day=new Date().getDay();
+console.log(day);
 if(new Date().getHours()>=0 && new Date().getHours()<6 ){
     document.body.style.backgroundColor='black'
-alert(`Gecen xeyre ${name} muellim!`);
+alert(`bugun:${day}-cu gun Gecen xeyre ${name} muellim!`);
 
 }
 
 else if(new Date().getHours()>=6 && new Date().getHours()<12 ){
     document.body.style.backgroundColor='white'
-alert(`Sabahin xeyir ${name} muellim!`);
-
+alert(`bugun:${day}-cu gun Sabahin xeyir ${name} muellim!`);
 }
 
 else if(new Date().getHours()>=12 && new Date().getHours()<18 ){
     document.body.style.backgroundColor='yellow'
-alert(`Gunortan xeyir ${name} muellim!`);
+alert(`bugun:${day}-cu gun Gunortan xeyir ${name} muellim!`);
 
 }
 
 else if(new Date().getHours()>=18 && new Date().getHours()<24 ){
     document.body.style.backgroundColor='grey'
-alert(`Axsamin xeyir ${name} muellim!`);
+alert(`bugun:${day}-cu gun Axsamin xeyir ${name} muellim!`);
 
 }
 
 else{
-    alert(`Basqa bir ehtimal yoxdur`);
+    alert(`${day}-cu gun ucun basqa bir ehtimal yoxdur`);
 }
 
 
